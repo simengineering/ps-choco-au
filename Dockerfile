2070a583693d33c3b46033ccd1fa5adc15c0e314
Matthew L Hopper <matthew.l.hopper@saic.com>
2024-02-11 13:20:15 -0500

    added files to repo

---
+++ Dockerfile
@@ -0,0 +1,52 @@
+FROM mono:6.12.0
+
+LABEL name="choco-au"
+
+ADD configure.sh /tmp
+
+RUN cd /tmp && chmod 777 configure.sh && ./configure.sh
+
+#RUN su opc
+
+RUN echo "alias powershell='/usr/bin/pwsh'" >> ~/.bashrc
+
+ARG CHOCOVERSION=stable
+
+# Download, extract, and move chocolatey installer
+WORKDIR /usr/local/src
+RUN wget "https://github.com/chocolatey/choco/archive/${CHOCOVERSION}.tar.gz" && \
+    tar -xzf "${CHOCOVERSION}.tar.gz" && \
+    rm "${CHOCOVERSION}.tar.gz" && \
+    mv "choco-${CHOCOVERSION}" choco
+
+# Build chocolatey
+WORKDIR /usr/local/src/choco
+RUN chmod +x build.sh zip.sh
+RUN ./build.sh -v
+
+# Symlink the build output to our install directory
+RUN ln -s /usr/local/src/choco/code_drop/chocolatey /opt/chocolatey
+
+# Copy in the choco helper script
+COPY bin/choco /usr/bin/choco
+
+ENV ChocolateyInstall /opt/chocolatey
+
+RUN choco install au -y 
+
+RUN echo 'Install-Module -Name AU -RequiredVersion 2020.11.21 -Force' | pwsh
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
---
+++ configure.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Update the list of packages
+apt-get update
+
+# Install pre-requisite packages.
+apt-get install -y wget
+
+# Get the version of Debian
+source /etc/os-release
+
+# Download the Microsoft repository GPG keys
+wget -q https://packages.microsoft.com/config/debian/$VERSION_ID/packages-microsoft-prod.deb
+
+# Register the Microsoft repository GPG keys
+dpkg -i packages-microsoft-prod.deb
+
+# Delete the Microsoft repository GPG keys file
+rm packages-microsoft-prod.deb
+
+# Update the list of packages after we added packages.microsoft.com
+apt-get update
+
+# Install PowerShell
+apt-get install -y powershell
+
+# prep for fips 140-2 openssl cryptographic modules 140-2 openssl cryptographic modules 140-2 openssl cryptographic modules 140-2 openssl cryptographic modules 140-2 openssl cryptographic modules 140-2 openssl cryptographic modules 140-2 openssl cryptographic modules 140-2 openssl cryptographic modules 140-2 openssl cryptographic modules 140-2 openssl cryptographic modules 140-2 openssl cryptographic modules 140-2 openssl cryptographic modules 140-2 openssl cryptographic modules 140-2 openssl cryptographic modules install
+apt-get install -y gcc
+apt-get install -y make
+
+# deal with the fips install business...
+wget https://www.openssl.org/source/openssl-3.0.8.tar.gz
+tar -xf openssl-3.0.8.tar.gz
+cd openssl-3.0.8
+./Configure enable-fips
+make
+
+
